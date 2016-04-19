c50d986e0e6c4728c94ab040f5308d86472ce506
Sri Datla <cybrchitti@gmail.com>
2016-04-19 23:33:37 +0100

    Major Overhaul Polymer --> Materialise
    — No more polymer
    — No more bower
    ++ Materialise css (easier to use)
    ++File system changed
    ++Media changed

---
+++ js/init.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function() {
+	$(".button-collapse").sideNav({
+		menuWidth: 240,
+		edge: 'left',
+		closeOnClick: true
+	});	
+	
+	$('.parallax').parallax();
+	$('.scrollspy').scrollSpy();
+}) 
